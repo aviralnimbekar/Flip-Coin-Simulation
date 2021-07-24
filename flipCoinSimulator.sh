416fd1567af9c9039f4aa5f175ff384392ed85d2
amitnimbekar <amit.nimbekar@actifio.com>
2021-07-25 04:32:33 +0530

    Flipping coin till one reaches to 21

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -5,19 +5,28 @@ echo "Welcome to Flip Coin Simulation Program"
 head=0
 Hcount=0
 Tcount=0
-counter=0
 
-while [ $counter -lt 21 ]
+while [ True ] 
 do
-	counter=$(($counter + 1))
 	randomCheck=$((RANDOM % 2))
 	if [ $randomCheck == 0 ]
 	then
 		echo "HEADS"
 		Hcount=$(($Hcount + 1))
+		
+		if [ $Hcount == 21 ]
+		then
+			break
+		fi
 	else
 		echo "TAILS"
 		Tcount=$(($Tcount + 1))
+	
+		if [ $Tcount == 21 ]
+                then
+                        break
+                fi
+
 	fi
 done
 
